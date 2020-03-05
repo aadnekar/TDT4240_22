bf070210476b926ba57d96b786176c765339168a
Ådne <aadne.karstad@gmail.com>
2020-03-05 09:02:32 +0100

    Generate LibGDX project

---
+++ game/android/src/ntnu/gruppe22/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ntnu.gruppe22.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ntnu.gruppe22.game.AnimalWar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AnimalWar(), config);
+	}
+}
---
+++ game/desktop/src/ntnu/gruppe22/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ntnu.gruppe22.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ntnu.gruppe22.game.AnimalWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AnimalWar(), config);
+	}
+}
