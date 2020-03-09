24c22c513f1568bb4414bdabcde5ff68008cb1c5
Åse Håtveit <aasehaa@stud.ntnu.no>
2020-03-09 13:31:10 +0100

    Modified AnimalWar - light version, no autosizing

--- game/core/src/ntnu/gruppe22/game/AnimalWar.java
+++ game/core/src/ntnu/gruppe22/game/AnimalWar.java
@@ -6,28 +6,35 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
+import ntnu.gruppe22.game.states.GameStateManager;
+
 public class AnimalWar extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	public static String APP_TITLE = "Animal War";
+	public static final int HEIGHT = 480;
+	public static final int WIDTH = 800;
+
+	private SpriteBatch sb;
+	private GameStateManager gsm;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		sb = new SpriteBatch();
+		gsm = new GameStateManager();
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime()); //difference between render times
+		gsm.render(sb);
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+	public void dispose () { sb.dispose(); }
+
+
 }
