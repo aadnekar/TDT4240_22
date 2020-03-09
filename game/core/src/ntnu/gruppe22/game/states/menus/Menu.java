0db0518152d95217212a8dfbf4945ad82805198d
Ådne <aadne.karstad@gmail.com>
2020-03-09 12:51:22 +0100

    Skeleton of class and render method

---
+++ game/core/src/ntnu/gruppe22/game/states/menus/Menu.java
@@ -0,0 +1,49 @@
+package ntnu.gruppe22.game.states.menus;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import ntnu.gruppe22.game.AnimalWar;
+import ntnu.gruppe22.game.states.State;
+
+/**
+ * @author aadne on 06.03.2020 21:47
+ */
+
+public abstract class Menu extends State {
+
+    final AnimalWar game;
+
+    public Menu(GameStateManager gameStateManager) {
+        super(gameStateManager);
+
+        this.camera.setToOrtho(false, 800, 400);
+    }
+
+
+    @Override
+    public void render(SpriteBatch sb) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        this.camera.update();
+        sb.setProjectionMatrix(camera.combined);
+
+        sb.begin();
+        // Should be some font to user.
+        // font.draw(sb, "Welcome to AnimalWar", pos.x, pos.y);
+        sb.end();
+
+     /*   // if (Button.clicked()) {
+        this.gameStateManager.set(new MainGame());
+        this.dispose();
+    }*/
+
+
+    }
+
+
+}
