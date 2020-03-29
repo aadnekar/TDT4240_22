c8ce645003dfce88aeccbd56162e0121a0fe2fd4
Jane <janewl@ntnu.stud.no>
2020-03-29 19:08:49 +0200

    adding first version of game maps with Box2D collision detection

---
+++ game/android/assets/map/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="background" tilewidth="128" tileheight="128" tilecount="35" columns="7">
+ <image source="background.png" width="1000" height="750"/>
+</tileset>
---
+++ game/android/assets/map/foresttileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="foresttileset" tilewidth="64" tileheight="64" tilecount="35" columns="7">
+ <image source="foresttileset.jpg" width="490" height="368"/>
+</tileset>
