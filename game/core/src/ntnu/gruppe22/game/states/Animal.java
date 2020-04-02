daa5b98a880fdc89fcf941efc3045b3ac769e787
Ådne <aadne.karstad@gmail.com>
2020-04-02 12:22:46 +0200

    Delete unused code.
    Makes it easier to develop on the fly. Also makes it easier to change ideas when we're in the development phase.

--- game/core/src/ntnu/gruppe22/game/states/Animal.java
+++ game/core/src/ntnu/gruppe22/game/states/Animal.java
@@ -11,16 +11,13 @@ import javax.xml.soap.Text;
 
 
 public class Animal extends Sprite {
-
     private int health;
     private int endurance;
-    //private ArrayList<Weapon> inventory = new ArrayList<Weapon>();
-    //private Weapon equippedWeapon;
-
     private float speed = 5f;
 
     private Texture animalTexture;
     private NinePatch healthbar;
+
 
     public Animal(int animalKey) {
         super(new Texture(Gdx.files.internal(GameRules.getAnimalTexture(animalKey))));
@@ -44,15 +41,6 @@ public class Animal extends Sprite {
         }
     }
 
-    public Texture getAnimalTexture(){
-        return animalTexture;
-    }
-    public Texture getBarTexture(){return healthbar.getTexture();}
-
-    private void updateEndurance() {
-        //??
-    }
-
     //when hit by weapon that deals x damage
     public void setHealth(int damage) {
         this.health = damage;
@@ -68,39 +56,16 @@ public class Animal extends Sprite {
         // remove this Animal after ... time (maybe add a animation? )
     }
 
-
     public int getHealth() {
         return health;
     }
-
+    
+    public int getEndurance() {
+        return endurance;
+    }
 
     public void draw(Batch batch) {
         super.draw(batch);
         batch.draw(healthbar.getTexture(), this.getX() + 5, this.getY() + 110);
     }
-
-
-/*
-    public ArrayList<Weapon> getInventory() {
-        return inventory;
-    }
-
-    public void addToInventory(Weapon weapon) {
-        inventory.add(weapon);
-    }
-
-    public int getEndurance() {
-        return endurance;
-    }
-
-    public void setEndurance(int endurance) {
-        this.endurance = endurance;
-    }
-
-    public void setEquippedWeapon(Weapon equippedWeapon) {
-        this.equippedWeapon = equippedWeapon;
-    }
-
- */
-
 }
