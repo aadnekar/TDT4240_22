95af0884314bab971179be710b0122843fcb32aa
Kristian Bondevik Hardang <khardang@gmail.com>
2020-04-16 11:04:30 +0200

    Create a super class for Menu and extend into existing button classes

--- game/core/src/ntnu/gruppe22/game/scenes/SelectScreen.java
+++ game/core/src/ntnu/gruppe22/game/scenes/SelectScreen.java
@@ -21,93 +21,35 @@ import ntnu.gruppe22.game.huds.SelectScreenButtons;
  * Importerer metoder som render(), update(), dispose(), handleInput()
  */
 
-
-//extend abstract class in some way?
-public class SelectScreen implements Screen {
-
-
-    private AnimalWar game;
-    private OrthographicCamera camera;
-    private SelectScreenButtons screenBtns;
-    private Viewport gameViewport;
-    private Texture bg;
-    private HashMap<Integer, ArrayList<Integer>> animalChoices;
-
-
-
-
+public class SelectScreen extends Menu{
 
     public SelectScreen(AnimalWar game) {
-        //selectedCharacter = characters.get(0); //henter ut første i lista?
-
-        this.game = game;
-        this.camera = new OrthographicCamera();
-        this.camera.setToOrtho(false, GameInfo.WIDTH, GameInfo.HEIGHT);
-        this.camera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
-
-
-        gameViewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, camera);
-
-        screenBtns = new SelectScreenButtons(game);
-
-        bg = new Texture("backgrounds/menu-bg.png");
-
+        this.initializeMenu(game);
+        btns = new SelectScreenButtons(game);
 
     }
 
-
-
-    @Override
-    public void show () {
-        
-
-    }
-
-    @Override
     public void render ( float delta){
-
-
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.getSb().begin();
-
         game.getSb().draw(bg, 0, 0);
-
         game.getSb().end();
 
-        game.getSb().setProjectionMatrix(screenBtns.getStage().getCamera().combined);
-        screenBtns.getStage().draw();
-        screenBtns.getStage().act();
-
-
+        game.getSb().setProjectionMatrix(btns.getStage().getCamera().combined);
+        btns.getStage().draw();
+        btns.getStage().act();
     }
 
-    @Override
     public void resize ( int width, int height){
         gameViewport.update(width, height);
     }
 
-    @Override
-    public void pause () {
-
-    }
-
-    @Override
-    public void resume () {
-
-    }
-
-    @Override
-    public void hide () {
-
-    }
-
-    @Override
     public void dispose () {
         Gdx.input.setInputProcessor(null);
         bg.dispose();
-        screenBtns.disposeStage();
+        btns.disposeStage();
     }
 }
 
