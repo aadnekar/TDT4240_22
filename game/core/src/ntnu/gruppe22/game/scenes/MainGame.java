95cbf7086dba3cfd8ba30050d1191f05045b305a
henedreg <henedreg@stud.ntnu.no>
2020-04-17 10:43:32 +0200

    Added healthbar

--- game/core/src/ntnu/gruppe22/game/scenes/MainGame.java
+++ game/core/src/ntnu/gruppe22/game/scenes/MainGame.java
@@ -229,11 +229,12 @@ public class MainGame implements Screen {
 
     //programmet stopper i ca 1 sek før steinen forsvinner
     private void destroyWeapon() {
+        /*
         try {
-            Thread.sleep(800); //bedre måte å løse dette på? Tror ikke dette er helt optimalt
+            Thread.sleep(800);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
         world.destroyBody(stone.b2body);
         stone = null;
         DestroyWeapon = false;
