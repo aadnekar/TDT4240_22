3ad595ed14f6f87e576fd6abc8e8b8fa5293adb6
unknown <janewl@stud.ntnu.no>
2020-04-27 20:37:43 +0200

    reverted changes in createUserButtons

--- game/core/src/ntnu/gruppe22/game/huds/CreateUserButtons.java
+++ game/core/src/ntnu/gruppe22/game/huds/CreateUserButtons.java
@@ -24,6 +24,8 @@ public class CreateUserButtons extends Buttons {
     private static CreateUserInput listener;
     private static CreateUserInput listener2;
 
+    private String nick1, nick2;
+
     public CreateUserButtons(AnimalWar game) {
         this.initializeButtons(game);
 
@@ -57,6 +59,7 @@ public class CreateUserButtons extends Buttons {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 Gdx.input.getTextInput(listener, "Enter information", "", "");
+                nick1 = listener.text;
 
             }
         });
@@ -64,6 +67,7 @@ public class CreateUserButtons extends Buttons {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 Gdx.input.getTextInput(listener2, "Enter information", "", "");
+                nick2 = listener2.text;
             }
         });
         continueToGame.addListener(new ChangeListener() {
